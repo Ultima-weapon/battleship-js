4226332e8dc942da9c0b66e28a8651a6ad48b28d
Andrew Gould <agould45@ku.edu>
2021-02-15 14:54:42 -0600

    Added generation of buttons on valid input. Fixed some formatting errors with jQuery append, not sure why they were happening but some CSS seems to have fixed it, might require some looking into further down the line.

--- js/battleship.js
+++ js/battleship.js
@@ -1,9 +1,19 @@
+function addButtons(numberOfShips)
+{
+    for (i = 0; i < numberOfShips; i++)
+    {
+        var btnHTML = '<button id="btn' + (i+1) + '" class="button is-success selector-btn">1 x ' + (i + 1) + '</button>';
+        $("#ship-selector").append(btnHTML)
+    }
+}
+
 $("#btn-start-game").click(function() {
     let numberOfShips = parseInt($("#no-of-ships").val());
 
     if (numberOfShips <= 6 && numberOfShips > 0)
     {
         alert("Success");
+        addButtons(numberOfShips);
     } else {
         alert("Failure.");
         $("#no-of-ships-err").fadeIn(200);
