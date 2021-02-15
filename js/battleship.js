58eef6793640fd63694a624f20b9367fb791a492
Andrew Gould <agould45@ku.edu>
2021-02-15 16:46:04 -0600

    Added JavaScript generation of player boards.

--- js/battleship.js
+++ js/battleship.js
@@ -7,14 +7,28 @@ function addButtons(numberOfShips)
     }
 }
 
-function showPlayer1()
+function generateBoard(playerNo)
 {
-    $("#board-1").slideDown(400);
-}
+    console.log("Generating blank board...");
 
-function showPlayer2()
-{
+    $("#board").append("<table id='player-1' class='table is-bordered is-fullwidth has-text-centered'></table>");
+    for (var i = 0; i < 11; i++) {
+        $("#player-" + playerNo).append("<tr id='row-" + i + "'></tr>");
+        for (var j = 0; j < 11; j++) {
+            if (i == 0 && j == 0)
+            {
+                $("#row-" + i).append("<td id='origin'></td>");
+            } else if (i == 0) {
+                $("#row-" + i).append("<th>" + j +"</th>")
+            } else if (j == 0) {
+                $("#row-" + i).append("<th>" + String.fromCharCode(96 + i).toUpperCase() +"</th>")
+            } else {
+                $("#row-" + i).append("<td id='" + i.toString() + j.toString() + "'></td>");
+            }
+        }
+    }
 
+    console.log("Finished generating player board for " + playerNo + ".");
 }
 
 $("#btn-start-game").click(function() {
@@ -23,10 +37,11 @@ $("#btn-start-game").click(function() {
     if (numberOfShips <= 6 && numberOfShips > 0)
     {
         addButtons(numberOfShips);
+        generateBoard(1);
         $("#start-game").slideUp(400);
         $("#placement-options").slideDown(400);
-        $("#board-space").slideDown(400);
+        $("#board").slideDown(400);
     } else {
-        $("#no-of-ships-err").fadeIn(200);
+        $("#no-of-ships-err").slideDown(400);
     }
 });
