4342d8b4986878ace1c87e6a36e48962584f3e0c
Andrew Gould <agould45@ku.edu>
2021-02-15 15:34:00 -0600

    Added the animations for bringing the board and board placement controls into view once the game has started successfully.

--- js/battleship.js
+++ js/battleship.js
@@ -7,15 +7,26 @@ function addButtons(numberOfShips)
     }
 }
 
+function showPlayer1()
+{
+    $("#board-1").slideDown(400);
+}
+
+function showPlayer2()
+{
+
+}
+
 $("#btn-start-game").click(function() {
     let numberOfShips = parseInt($("#no-of-ships").val());
 
     if (numberOfShips <= 6 && numberOfShips > 0)
     {
-        alert("Success");
         addButtons(numberOfShips);
+        $("#start-game").slideUp(400);
+        $("#placement-options").slideDown(400);
+        $("#board-space").slideDown(400);
     } else {
-        alert("Failure.");
         $("#no-of-ships-err").fadeIn(200);
     }
 });
