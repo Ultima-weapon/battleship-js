1902262d465b5ee4019b31a6ca58c5f29d88bb4d
Andrew Gould <agould45@ku.edu>
2021-02-16 12:24:13 -0600

    Added logic to selector buttons to show selection and add disabled attribute to check selection.

---
+++ js/gameboard.js
@@ -0,0 +1,8 @@
+// Selector button logic
+$(document).on('click', ".selector-btn", function() {
+    $('#ship-selector').children('button').each(function () {
+        $("#" + this.id).attr("disabled", false);
+    });
+    $("#" + this.id).attr("disabled", true);
+});
+
