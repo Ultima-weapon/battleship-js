4606cfa7096cde1807f71696c8ed0b7b2274f041
Andrew Gould <agould45@ku.edu>
2021-02-18 14:12:42 -0600

    Added Player class constructor and test to check construction of players

--- js/__test__.js
+++ js/__test__.js
@@ -1,6 +1,10 @@
 let test_ship_x = new Ship(6, 'x', 11);
 let test_ship_y = new Ship(6, 'y', 11);
+let test_player_1 = new Player(1);
+let test_player_2 = new Player(2);
 
 console.log(test_ship_x);
 console.log(test_ship_y);
 console.log(test_ship_x.isSunk());
+console.log(test_player_1);
+console.log(test_player_2);
--- js/player.js
+++ js/player.js
@@ -1,4 +1,10 @@
 class Player
 {
-
+    constructor(playerID)
+    {
+        // Array to hold ships
+        this.ships = [];
+        // If Player 1, isTurn is true, if not, isTurn is false
+        this.isTurn = (playerID == 1 ? true : false);
+    }
 };
