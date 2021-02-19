a103d830eeb1b304117969251e8447336dc2b831
Andrew Gould <agould45@ku.edu>
2021-02-18 20:04:38 -0600

    Added basic skeleton for Game class

--- game.js
+++ game.js
@@ -1,4 +1,17 @@
 class Game
 {
+    constructor(players)
+    {
+        // States
+        // 1 = Placing
+        // 2 = Firing
+        // 3 = Game Over
+        this.state = 1;
+        this.players = players;
+    }
 
+    getState()
+    {
+        return this.state;
+    }
 };
