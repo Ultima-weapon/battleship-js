59f1420005ad17343925cc8074826aee2d1c2b09
Andrew Ford <24680647+Ultima-weapon@users.noreply.github.com>
2021-03-14 18:42:57 -0500

    ship.js Comment Documentation

--- js/ship.js
+++ js/ship.js
@@ -1,10 +1,22 @@
+/* -----------------------------------------------------------------------------
+ *
+ * File Name: ship.js
+ * Author: (Team 7 - Project 2 Team) & (Team 3 - Original Team)
+ * Assignment: EECS 448 - Project 2
+ * Description: Ship Class
+ * Date: 14 Mar 2020
+ *
+ ---------------------------------------------------------------------------- */
+ 
 class Ship
 {
-    /**
-     * Constructs a Ship class with the size of the ship and the ID of the cells it occupies
-     * @param {number} size - the size of the Ship
-     * @param {Array} cells - the ID of the cells the Ship occupies
-     */
+	/**
+	* @constructor
+	* @pre None
+	* @post CONSTRUCTOR: Constructs a Ship class with the size of the ship and the ID of the cells it occupies
+	* @param size, number representing the ship size.
+	* @return cells, array of cell IDs that the ship occupies.
+	**/
     constructor(size, cells) 
     {
         this.size = parseInt(size);
@@ -12,22 +24,23 @@ class Ship
         this.hits = [];
     }
 
-    /**
-     * Helper class that returns whether the ship has been sunk or not.
-     * @param None
-     * @returns Boolean - True if ship has been sunk, False is ship has not been sunk.
-     * @deprecated
-     */
+	/**
+	* @pre None
+	* @post PUBLIC METHOD [isSunk()]: Returns whether the ship has been sunk or not.
+	* @param None.
+	* @return [boolean], true if ship has been sunk.
+	**/
     isSunk() 
     {
         return (this.hits.length == this.size);
     }
 
-    /**
-     * Helper class that sets a cell of the ship to hit if it is one of the Ship's occupied cells.
-     * @param {number} hitCell 
-     * @deprecated
-     */
+	/**
+	* @pre None
+	* @post PUBLIC METHOD [hit(hitCell)]: Sets a cell of the ship to hit if it is one of the Ship's occupied cells.
+	* @param hitCell, number representing the ID of the cell hit.
+	* @return None
+	**/
     hit(hitCell)
     {
         for (cell in this.cells) {
