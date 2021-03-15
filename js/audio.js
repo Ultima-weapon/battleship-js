793407237cc63e6aa167f801eff28489d35553cd
Andrew Ford <24680647+Ultima-weapon@users.noreply.github.com>
2021-03-14 19:17:05 -0500

    audio.js Comment Documentation

--- js/audio.js
+++ js/audio.js
@@ -1,4 +1,19 @@
-// Create Audio playback events
+/* -----------------------------------------------------------------------------
+ *
+ * File Name: audio.js
+ * Author: Team 7
+ * Assignment: EECS 448 - Project 2
+ * Description: Sound Functionality
+ * Date: 14 Mar 2020
+ *
+ ---------------------------------------------------------------------------- */
+ 
+/**
+* @pre None
+* @post FUNCTION [sound(src)]: Creates an audio playback event
+* @param src, string representing the audio source file path.
+* @return None
+**/
 function sound(src){
 	this.sound = document.createElement("audio");
 	this.sound.src = src;
@@ -9,6 +24,8 @@ function sound(src){
 	this.play = function(){ this.sound.play(); console.log("Sound Triggered: " + src) };
 	this.stop = function(){ this.sound.pause(); console.log("Sound Paused: " + src) };
 }
+
+// Create playback events for each audio file.
 let soundHit = new sound("audio/hit.mp3");
 let soundSunk = new sound("audio/sunk.mp3");
 let soundMiss = new sound("audio/miss.mp3");
